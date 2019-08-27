947d4147fbcbd5933f2c0a6cad689c45b7f7b152
tik <bump130guitar@gmail.com>
2019-08-27 00:42:59 +0000

    user login!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  #SessionsHelperを取り込む
+  include SessionsHelper
+  
+  private
+  
+  #ログインしていない場合ログイン画面に遷移させる
+  def require_user_logged_in
+    unless logged_in?
+    redirect_to login_url
+    
+    end
+  end
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,11 @@
+module SessionsHelper
+ 
+ #現在ログインしているユーザー情報を取得する
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+end
